9f05e242fe1d1a5492d7fe6f284d1f470f5715b1
PCSpacex <49347160+PCSpacex@users.noreply.github.com>
2019-05-13 21:15:24 +0200

    Update mail-script.js

--- js/mail-script.js
+++ js/mail-script.js
@@ -2,12 +2,12 @@
 
      $(document).ready(function() {
         var form = $('#myForm'); // contact form
-        var submit = $('.submit-btn'); // submit button
+        var submit = $('.submit-btn');// submit button
         var alert = $('.alert-msg'); // alert div for show alert message
 
         // form submit event
         form.on('submit', function(e) {
-            e.preventDefault(); // prevent default form submit
+            e.preventDefault();// prevent default form submit
 
             $.ajax({
                 url: 'mail.php', // form action url
